823bf5e4126700418b5e3d349e215850202d41d4
Yuni-Q <lyh6425@gmail.com>
2020-09-10 15:58:55 +0900

    delete api

--- pages/api/users.ts
+++
@@ -1,9 +0,0 @@
-export default (req: any, res: any) => {
-	if (req.method === 'POST') {
-		// Process a POST request
-	}
-	// Handle any other HTTP method
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify({ name: 'Yuni-Q' }));
-};
